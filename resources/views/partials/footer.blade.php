61d449e147a0f3af3eae71a5747e194ca1b54ea4
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-09-06 19:10:11 +0200

    add footer up

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,26 @@
+<footer>
+    <div class="footer-up">
+    <ul>
+        <li>
+            <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="digital comics">
+            <h4>digital comics</h4>
+        </li>
+        <li>
+            <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="merchandise">
+            <h4>dc merchandise</h4>
+        </li>
+        <li>
+            <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="subscriptions">
+            <h4>subscription</h4>
+        </li>
+        <li>
+            <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="shop locator">
+            <h4>comic shop locator</h4>
+        </li>
+        <li>
+            <img class="last-img" src="{{asset('img/buy-dc-power-visa.svg')}}" alt="dc power visa">
+            <h4>dc power visa</h4>
+        </li>
+    </ul>
+    </div>
+</footer>
