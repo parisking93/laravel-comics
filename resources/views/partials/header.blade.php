75d35449b4db0a06fae5dfb7bcf611ac2bdc01ca
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-09-06 17:22:28 +0200

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,29 @@
 <header>
-    <div class="container-logo"></div>
-    <nav>
-        <ul>
-            <li><a href="">characters</a></li>
-            <li><a href="{{ route('comics') }}">comics</a></li>
-            <li><a href="">movies</a></li>
-            <li><a href="">tv</a></li>
-            <li><a href="">games</a></li>
-            <li><a href="">collectibles</a></li>
-            <li><a href="">videos</a></li>
-            <li><a href="">fans</a></li>
-            <li><a href="">news</a></li>
-            <li><a href="">shop</a></li>
-        </ul>
-    </nav>
+    <div class="header-bar">
+
+    </div>
+    <div class="container-center">
+        <div class="container-logo">
+            <img src="{{asset('img/dc-logo.png')}}" alt="dc comics">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">characters</a></li>
+                <li><a href="{{ route('comics') }}">comics</a></li>
+                <li><a href="">movies</a></li>
+                <li><a href="">tv</a></li>
+                <li><a href="">games</a></li>
+                <li><a href="">collectibles</a></li>
+                <li><a href="">videos</a></li>
+                <li><a href="">fans</a></li>
+                <li><a href="">news</a></li>
+                <li><a href="">shop</a></li>
+            </ul>
+        </nav>
+        <div class="box-search-bar">
+            <input type="text" placeholder="Search">
+
+        </div>
+    </div>
+    <div class="jumbotron"></div>
 </header>
